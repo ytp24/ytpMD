aadc9977bca12906807692acec04757a7dca7b32
ytp24 <ykinwork24@gmail.com>
2026-08-26 03:10:24 +0400

    fix: remove svr package from vite config file

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
